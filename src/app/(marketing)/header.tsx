8158adc9161a731d934bd7e0df0c1db6afe9caf1
veda <vedabezaleel@gmail.com>
2025-02-04 05:00:04 +0700

    feat: Sync with clerk for auth

--- src/app/(marketing)/header.tsx
+++ src/app/(marketing)/header.tsx
@@ -1,5 +1,6 @@
 export default function Header() {
   return (
-    <header className="h-20 w-full border-b-2 border-slate-200 px-4">Header</header>
+    <header className="h-20 w-full border-b-2 border-slate-200 px-4"><div className="lg:max-w-screen-lg mx-auto flex items-center justify-between"><div className="pt-8 pl-4 pb-7 flex items-center gap-x-3">
+        </div></div></header>
   );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Nunito } from "next/font/google";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -14,8 +15,8 @@ const geistMono = Geist_Mono({
 
 const nunito = Nunito({
   variable: "--font-nunito",
-  subsets: ["latin"]
-})
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -28,12 +29,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} ${nunito.className} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} ${nunito.className} antialiased`}
+        >
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
