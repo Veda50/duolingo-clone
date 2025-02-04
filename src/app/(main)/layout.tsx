51d5e22d5045220ed8382dcaeae70aa0ec5c1b77
veda <vedabezaleel@gmail.com>
2025-02-05 02:33:02 +0700

    feat: learn page wrapper

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -11,7 +11,7 @@ export default function MainLayout({ children }: Props) {
     <MobileHeader />
       <Sidebar className="hidden lg:flex" />
       <main className="lg:pl-[256px] h-full pt-[50px] lg:pt-0">
-        <div className="h-full bg-green-500">{children}</div>
+        <div className="h-full max-w-[1056px] mx-auto pt-6">{children}</div>
       </main>
     </>
   );
--- src/app/(main)/learn/page.tsx
+++ src/app/(main)/learn/page.tsx
@@ -1,3 +1,26 @@
-export default function LearnPage(){
-    return (<div>Learn Page</div>)
+import FeedWrapper from "@/components/feed-wrapper";
+import StickyWrapper from "@/components/sticky-wrapper";
+import Header from "./header";
+import UserProgress from "@/components/user-progress";
+
+export default function LearnPage() {
+  return (
+    <div className="flex flex-row-reverse gap-[48px] px-6">
+      <StickyWrapper>
+        <UserProgress
+          activeCourse={{ title: "english", imageSrc: "/us.svg" }}
+          hearts={5}
+          points={100}
+          hasActiveSubscription={false}
+        />
+      </StickyWrapper>
+      <FeedWrapper>
+        <Header title="English" />
+        <div className="space-y-4">
+          <div className="h-[700px] bg-green-500 w-full"></div>
+          <div className="h-[700px] bg-green-500 w-full"></div>
+        </div>
+      </FeedWrapper>
+    </div>
+  );
 }
