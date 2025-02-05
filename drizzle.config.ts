d525748e870fcbc3cf6ab643c9cee5e42042b655
veda <vedabezaleel@gmail.com>
2025-02-06 01:33:04 +0700

    feat: neonDB and drizzle orm config

---
+++ drizzle.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from "drizzle-kit"
+
+// export default defineConfig{
+//     schema: "./db/schema.ts",
+//     out: "./drizzle",
+//     driver: "pglite",
+//     dbCredentials: {
+//         url: process.env.DATABASE_URL || "postgresql://neondb_"
+//     },
+//     dialect: "postgresql"
+// }
+
+export default defineConfig({
+    dialect: "postgresql",
+    schema: "./db/schema.ts",
+    // driver: "pg",
+    out: "drizzle",
+    dbCredentials: {
+        url: process.env.DATABASE_URL || "postgresql://neondb_"
+    }
+})
